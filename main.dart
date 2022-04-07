b4a0d1deab1587c72292d43481096fac33471e4a
hongyupyi <102046575+hongyupyi@users.noreply.github.com>
2022-04-07 18:53:59 +0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,112 +1,57 @@
 import 'package:flutter/material.dart';
-
-class ToDo {
-  bool isDone = false; //current progress
-  String title; //works
-
-  ToDo(this.title);
-}
+import 'package:provider/provider.dart';
+import 'package:todo2/pages/todos_page.dart';
+import 'package:todo2/providers/providers.dart';
+import 'package:todo2/providers/todo_filter.dart';
+import 'package:todo2/providers/todo_list.dart';
+import 'package:todo2/providers/todo_search.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ToDo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: ToDoListPage(),
-    );
-  }
-}
-
-class ToDoListPage extends StatefulWidget {
-  @override
-  _ToDoListPageState createState() => _ToDoListPageState();
-}
-
-class _ToDoListPageState extends State<ToDoListPage> {
-  final _items = <ToDo>[]; // saving list of things to do
-  var _todoController = TextEditingController(); //Controller of saving list
-
-  void _addTodo(ToDo toDo) { //Add things to do
-    setState(() {
-      _items.add(toDo);
-      _todoController.text = '';
-    });
-  }
-
-  void _deleteTodo(ToDo toDo) { //delete things to do
-    setState(() {
-      _items.remove(toDo);
-    });
-  }
-
-  void _toggleTodo(ToDo toDo) { //check things to do
-    setState(() {
-      toDo.isDone = !toDo.isDone;
-    });
-  }
-
-  @override
-  void dispose() {
-    _todoController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Things to do'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: TextField(
-                      controller: _todoController,
-                    ),
-                ),
-                RaisedButton(
-                    onPressed: () => _addTodo(ToDo(_todoController.text)),
-                    color: Colors.indigo,
-                    child: Text('add', style: TextStyle(color: Colors.white),),
-                    )
-                  ],
-                ),
-                Expanded(
-                    child: ListView(
-                      children: _items.map((todo) => _buildItemWidget(todo)).toList(),
-                ),
-                )
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (context) => TodoFilter(),
         ),
-      ),
-    );
-  }
-
-  Widget _buildItemWidget(ToDo toDo) {
-    return ListTile(
-      onTap: () => _toggleTodo(toDo), // Complete/Cancel if click
-      trailing: IconButton(
-        icon: Icon(Icons.delete),
-        onPressed: () => _deleteTodo(toDo), //delete things to do
-      ),
-      title: Text(
-        toDo.title,
-        style: toDo.isDone? //Complete or not
-          TextStyle(
-            decoration: TextDecoration.lineThrough, //Cancel line
-            fontStyle: FontStyle.italic,
-          ) : null,
+        ChangeNotifierProvider<TodoSearch>(
+          create: (context) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (context) => TodoList(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(initialActiveTodoCount: context.read<TodoList>().state.todos.length),
+          update: (BuildContext context, TodoList todoList, ActiveTodoCount? activeTodoCount,) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter,
+            TodoSearch,
+            TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(initialFilteredTodos: context.read<TodoList>().state.todos,),
+              update: (BuildContext context,
+              TodoFilter todoFilter,
+              TodoSearch todoSearch,
+              TodoList todoList,
+              FilteredTodos? filteredTodos) =>
+              filteredTodos!..update(todoFilter, todoSearch, todoList),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Todo Provider',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const TodosPage(),
       ),
     );
   }
